349d418bea8292fefc8c57e942d6ef0fdf54fe69
huaniaofengyue67@gmail.com <huaniaofengyue67@gmail.com>
2020-03-25 10:29:53 +0800

    菜品模型更新

--- Menu/admin.py
+++ Menu/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from Menu.models import FoodClassification,Dishes
 
-# Register your models here.
+class FoodClassificationAdmin(admin.ModelAdmin):
+	pass
+
+class DishesAdmin(admin.ModelAdmin):
+	pass
+
+
+admin.site.register(FoodClassification, FoodClassificationAdmin)
+admin.site.register(Dishes, DishesAdmin)
--- Menu/models.py
+++ Menu/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.db.models.signals import post_init,post_save,post_delete
+from django.dispatch import receiver
+from lxml import etree
+from django.conf import settings
+import os
 
 # Create your models here.
+# 菜品分类模型
 class FoodClassification(models.Model):
-	name = models.Charfield(verbose_name='菜品分类',max_length = 50)
+	name = models.CharField(verbose_name='菜品分类',max_length = 50)
 
 	def __str__ (self):
 		return self.name
@@ -11,9 +17,10 @@ class FoodClassification(models.Model):
 		verbose_name = '菜品分类'
 		verbose_name_plural = '菜品分类'
 
+# 菜品模型
 class Dishes(models.Model):
 	classification = models.ForeignKey('FoodClassification',on_delete=models.CASCADE)
-	name = models.Charfield(verbose_name='菜品名称',max_length = 50)
+	name = models.CharField(verbose_name='菜品名称',max_length = 50)
 	price = models.FloatField(verbose_name='价格')
 	discount_price = models.FloatField(verbose_name='折扣价格',blank = True)
 	img = models.ImageField(verbose_name='菜品图片',upload_to='dishes/')
@@ -25,3 +32,44 @@ class Dishes(models.Model):
 	class Meta:
 		verbose_name = '菜品'
 		verbose_name_plural = '菜品'
+
+# 菜品模型初始化信号
+@receiver(post_init, sender=Dishes)
+def img_init(sender,instance,**kwargs):
+	instance._bak_img = instance.img
+
+# 菜品模型保存信号
+@receiver(post_save, sender=Dishes)
+def img_change(sender,instance,**kwargs):
+	if instance._bak_img:
+		if instance.img != instance._bak_img:
+			file_url = os.path.join(settings.MEDIA_ROOT, str(instance._bak_img))
+			if os.path.isfile(file_url):
+				os.remove(file_url)
+				del_empty_dir(settings.MEDIA_ROOT)
+
+# 菜品模型删除信号
+@receiver(post_delete, sender=Dishes)
+def delete_upload_files(sender, instance, **kwargs):
+	del_url = []
+	file_url = os.path.join(settings.MEDIA_ROOT, str(instance.img))	
+	# content_html = etree.HTML(instance.content)
+	# content_img_src = content_html.xpath('//img/@src')
+	# for img_src in content_img_src:
+	# 	img_src = os.path.join(settings.BASE_DIR, img_src[1:])
+	# 	del_url.append(os.path.join(settings.BASE_DIR, img_src).replace('\\','/'))
+	del_url.append(file_url.replace('\\','/'))
+	for durl in del_url:
+		if os.path.isfile(durl):
+			os.remove(durl)
+
+	del_empty_dir(settings.MEDIA_ROOT)
+
+# 删除空的文件夹
+def del_empty_dir(dir):
+	if os.path.isdir(dir):
+		for item in os.listdir(dir):
+			del_empty_dir(os.path.join(dir,item))
+
+		if not os.listdir(dir):
+			os.rmdir(dir)
