33ff565f99fb8ca4790e30b20ffa06e9a44f3398
huaniaofengyue67@gmail.com <huaniaofengyue67@gmail.com>
2020-04-09 14:49:38 +0800

    修改使用视图集

--- Menu/urls.py
+++ Menu/urls.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path,include,re_path
 from Menu import views
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'food_classification', views.FoodClassificationViewSet)
+router.register(r'dishes', views.DishesViewSet)
+router.register(r'dishes_page', views.DishesPageViewSet)
 
 urlpatterns = [
 	path('dishes_management/', views.dishes_management),
-	path('dishes/', views.DishesList.as_view()),
-	path('dishes/<int:pk>', views.DishesDetail.as_view()),
-	path('dishes_page/', views.DisheListPage.as_view()),
-	path('food_classification/', views.FoodClassificationList.as_view()),
-	path('food_classification/<int:pk>', views.FoodClassificationDetail.as_view()),
+	# path('dishes/', views.DishesList.as_view()),
+	# path('dishes/<int:pk>', views.DishesDetail.as_view()),
+	# path('dishes_page/', views.DisheListPage.as_view()),
+	re_path(r'^', include(router.urls)),
+	# path('food_classification/', views.FoodClassificationList.as_view()),
+	# path('food_classification/<int:pk>', views.FoodClassificationDetail.as_view()),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- Menu/views.py
+++ Menu/views.py
@@ -2,91 +2,105 @@ from django.shortcuts import render
 from Menu.models import Dishes,FoodClassification
 from django.shortcuts import render
 from Menu.serializers import DishesSerializers,FoodClassificationSerializers,StandardResultsSetPagination
-from rest_framework import mixins
-from rest_framework import generics
+from rest_framework import mixins,generics,viewsets
 
 def dishes_management(request):
 	return render(request, 'menu/index.html')
 
 
-class FoodClassificationList(mixins.ListModelMixin,
-							mixins.CreateModelMixin,
-							generics.GenericAPIView):
-	model = FoodClassification
-	serializer_class = FoodClassificationSerializers
+class FoodClassificationViewSet(viewsets.ModelViewSet):
+	queryset = FoodClassification.objects.all()
+	serializer_class = FoodClassificationSerializers 
 
-	def get_queryset(self, *args, **kwargs):
-		return self.model.objects.all()
+class DishesViewSet(viewsets.ModelViewSet):
+	queryset = Dishes.objects.all()
+	serializer_class = DishesSerializers 
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
 
-	def post(self, request, *args, **kwargs):
-		return self.create(request, *args, **kwargs)
-
-class FoodClassificationDetail(mixins.RetrieveModelMixin,
-					mixins.UpdateModelMixin,
-					mixins.DestroyModelMixin,
-					generics.GenericAPIView):
-	model = FoodClassification
-	serializer_class = FoodClassificationSerializers
-
-	def get_queryset(self, *args, **kwargs):
-		return self.model.objects.all()
-
-	def get(self, request, *args, **kwargs):
-		return self.retrieve(request, *args, **kwargs)
-
-	def put(self, request, *args, **kwargs):
-		return self.update(request, *args, **kwargs)
-
-	def delete(self, request, *args, **kwargs):
-		return self.destroy(request, *args, **kwargs)
-
-class DisheListPage(mixins.ListModelMixin,
-					generics.GenericAPIView):
-	model = Dishes
-	serializer_class = DishesSerializers
+class DishesPageViewSet(viewsets.ModelViewSet):
+	queryset = Dishes.objects.all()
+	serializer_class = DishesSerializers 
 	pagination_class = StandardResultsSetPagination
 
-	def get_queryset(self, *args, **kwargs):
-		return self.model.objects.all()
+# class FoodClassificationList(mixins.ListModelMixin,
+# 							mixins.CreateModelMixin,
+# 							generics.GenericAPIView):
+# 	model = FoodClassification
+# 	serializer_class = FoodClassificationSerializers
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
+# 	def get_queryset(self, *args, **kwargs):
+# 		return self.model.objects.all()
 
-class DishesList(mixins.ListModelMixin,
-				mixins.CreateModelMixin,
-				generics.GenericAPIView):
+# 	def get(self, request, *args, **kwargs):
+# 		return self.list(request, *args, **kwargs)
 
-	model = Dishes
-	serializer_class = DishesSerializers
+# 	def post(self, request, *args, **kwargs):
+# 		return self.create(request, *args, **kwargs)
 
-	def get_queryset(self, *args, **kwargs):
-		return self.model.objects.all()
+# class FoodClassificationDetail(mixins.RetrieveModelMixin,
+# 					mixins.UpdateModelMixin,
+# 					mixins.DestroyModelMixin,
+# 					generics.GenericAPIView):
+# 	model = FoodClassification
+# 	serializer_class = FoodClassificationSerializers
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
+# 	def get_queryset(self, *args, **kwargs):
+# 		return self.model.objects.all()
 
-	def post(self, request, *args, **kwargs):
-		return self.create(request, *args, **kwargs)
+# 	def get(self, request, *args, **kwargs):
+# 		return self.retrieve(request, *args, **kwargs)
 
-class DishesDetail(mixins.RetrieveModelMixin,
-					mixins.UpdateModelMixin,
-					mixins.DestroyModelMixin,
-					generics.GenericAPIView):
-	model = Dishes
-	serializer_class = DishesSerializers
+# 	def put(self, request, *args, **kwargs):
+# 		return self.update(request, *args, **kwargs)
 
-	def get_queryset(self, *args, **kwargs):
-		return self.model.objects.all()
+# 	def delete(self, request, *args, **kwargs):
+# 		return self.destroy(request, *args, **kwargs)
 
-	def get(self, request, *args, **kwargs):
-		return self.retrieve(request, *args, **kwargs)
+# class DisheListPage(mixins.ListModelMixin,
+# 					generics.GenericAPIView):
+# 	model = Dishes
+# 	serializer_class = DishesSerializers
+# 	pagination_class = StandardResultsSetPagination
 
-	def put(self, request, *args, **kwargs):
-		print(request.data,args,kwargs)
-		return self.partial_update(request, *args, **kwargs)
+# 	def get_queryset(self, *args, **kwargs):
+# 		return self.model.objects.all()
 
-	def delete(self, request, *args, **kwargs):
-		return self.destroy(request, *args, **kwargs)
+# 	def get(self, request, *args, **kwargs):
+# 		return self.list(request, *args, **kwargs)
+
+# class DishesList(mixins.ListModelMixin,
+# 				mixins.CreateModelMixin,
+# 				generics.GenericAPIView):
+
+# 	model = Dishes
+# 	serializer_class = DishesSerializers
+
+# 	def get_queryset(self, *args, **kwargs):
+# 		return self.model.objects.all()
+
+# 	def get(self, request, *args, **kwargs):
+# 		return self.list(request, *args, **kwargs)
+
+# 	def post(self, request, *args, **kwargs):
+# 		return self.create(request, *args, **kwargs)
+
+# class DishesDetail(mixins.RetrieveModelMixin,
+# 					mixins.UpdateModelMixin,
+# 					mixins.DestroyModelMixin,
+# 					generics.GenericAPIView):
+# 	model = Dishes
+# 	serializer_class = DishesSerializers
+
+# 	def get_queryset(self, *args, **kwargs):
+# 		return self.model.objects.all()
+
+# 	def get(self, request, *args, **kwargs):
+# 		print(kwargs)
+# 		return self.retrieve(request, *args, **kwargs)
+
+# 	def put(self, request, *args, **kwargs):
+# 		print(request.data,args,kwargs)
+# 		return self.partial_update(request, *args, **kwargs)
+
+# 	def delete(self, request, *args, **kwargs):
+# 		return self.destroy(request, *args, **kwargs)
